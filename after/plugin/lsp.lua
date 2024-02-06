9750123422c96b1c75ed76a668fa5be4fa5e7291
Patrick Parker <patrickjpark@gmail.com>
2024-02-05 20:00:27 -0700

    Configure lsp with global vim variable

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -6,6 +6,17 @@ lsp_zero.on_attach(function(client, bufnr)
 	lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
+-- Fix Undefined global 'vim'
+lsp_zero.configure('lua_ls', {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' },
+      },
+    },
+  },
+})
+
 -- here you can setup the language servers 
 require('mason').setup({})
 require('mason-lspconfig').setup({
